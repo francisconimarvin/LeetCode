b2eedf3cac85380729e9ee0f37132cf2a374d001
Marvin Francisconi <125971672+francisconimarvin@users.noreply.github.com>
2026-06-23 19:00:42 +0000

    Create longestUniqueSubstring.c

---
+++ longestUniqueSubstring.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+// Sliding window approach (can also use set)
+
+int lengthOfLongestSubstring(char* s) {
+    // ASCII conversion (it may be 256)
+    int charMap[128] = {0}; // Hashmap simulation
+    int left = 0;
+    int maxLength = 0;
+    
+    for (int right = 0; s[right] != '\0'; right++) {
+        char currChar = s[right];
+        charMap[currChar]++;
+        
+        // Update window if found the same character
+        while (charMap[currChar] > 1) {
+            char caracter_izquierdo = s[left];
+            charMap[caracter_izquierdo]--; 
+            left++;
+        }
+        
+        // Current length
+        int currLength = right - left + 1;
+        
+        // Compare maxLength to currLength
+        if (currLength > maxLength) {
+            maxLength = currLength;
+        }
+    }
+    return maxLength;
+}
