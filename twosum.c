cec5f025893a9684f0b8c0515a054dd9a74216bd
Marvin Francisconi <125971672+francisconimarvin@users.noreply.github.com>
2026-06-26 21:03:07 +0000

    Update twosum.c

--- twosum.c
+++ twosum.c
@@ -2,11 +2,12 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    // Allocate memory for the return array 
+    // Derefer the pointer
     *returnSize = 2;
-    int *result = (int *)malloc(*returnSize * sizeof(int));
 
-    // Iterate the given list to get the target sum.
+    // Dynamic
+    int *result = (int*)malloc(*returnSize * sizeof(int)); // 2 * 4 = 8 bytes
+    
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
