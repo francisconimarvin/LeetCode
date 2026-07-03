e843262402dca0909ab3fc473ef8513a900ba200
Marvin Francisconi <125971672+francisconimarvin@users.noreply.github.com>
2026-07-03 01:42:00 +0000

    Create quickSort.c

---
+++ quickSort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int partition(int *arr, int low, int high) {
+    int pivot = arr[high];
+
+    int i = low - 1;
+    for (size_t j = low; j < high; j++) {
+        if (arr[j]<pivot) {
+            i++;
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    int temp = arr[high];
+    arr[high] = arr[i+1];
+    arr[i+1] = temp;
+    
+    return i+1;
+}
+void quickSort(int *arr, int low, int high) {
+    if (low > high) {
+        return;
+    }
+    
+    int pivot = partition(arr, low, high);
+
+    // Left
+    quickSort(arr, low, pivot-1);
+    quickSort(arr, pivot+1, high);
+}
+
+int main (int argv, char *argc[argv+1]) {
+    int arr[7] = {10, 6, 3, 4, 9, 10, 7};
+    int len = 7;
+    quickSort(arr, 0, len-1);
+    
+    for (int i = 0; i < len; i++) {
+        printf("Element %d:%d\n", i, arr[i]);
+    }
+}
+    
